e0df2d2a9cb35df9f1b6786f0be7d84c70489601
dinhhuutai <dinhhuutai20023107@gmail.com>
2025-05-18 14:06:16 +0700

    upload

--- src/db.js
+++ src/db.js
@@ -1,14 +1,21 @@
-const sql = require("mssql/msnodesqlv8");
+require('dotenv').config();
+const sql = require("mssql");
 
 const config = {
-  connectionString: "Driver={ODBC Driver 18 for SQL Server};Server=localhost;Database=DuAnRac;Trusted_Connection=Yes;TrustServerCertificate=Yes;"
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  server: process.env.SQL_SERVER,
+  database: process.env.SQL_DATABASE,
+  options: {
+    encrypt: process.env.SQL_ENCRYPT === 'true',
+    trustServerCertificate: false,
+  }
 };
-
 
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
   .then(pool => {
-    console.log("✅ Kết nối SQL Server thành công (Windows Auth)");
+    console.log("✅ Kết nối SQL Server thành công");
     return pool;
   })
   .catch(err => {
