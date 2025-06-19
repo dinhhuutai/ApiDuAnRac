7793f3ac954ecdc7fa4f6fa6e524e76538e604c0
dinhhuutai <dinhhuutai20023107@gmail.com>
2025-06-19 11:56:40 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.use(cors({
     'https://ashy-mud-0ac3ad21e.6.azurestaticapps.net',
     'http://localhost:3000',
     'https://noibo.thuanhunglongan.com',
+    'https://noibo.thuanhunglongan.com/login',
     'https://dashboardduanrac.netlify.app',
   ],
   credentials: true
