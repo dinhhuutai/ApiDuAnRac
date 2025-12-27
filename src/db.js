6ca2fc3ff8c71664840f45a04cc67d22bac371b5
dinhhuutai <dinhhuutai20023107@gmail.com>
2025-12-27 13:09:57 +0700

    upload

--- src/db.js
+++ src/db.js
@@ -1,37 +1,3 @@
-// require('dotenv').config();
-// const fs = require('fs');
-
-// const sql = require("mssql");
-
-// const config = {
-//   user: process.env.SQL_USER,
-//   password: process.env.SQL_PASSWORD,
-//   server: process.env.SQL_SERVER,
-//   database: process.env.SQL_DATABASE,
-//   options: {
-//     encrypt: process.env.SQL_ENCRYPT === 'true',
-//     trustServerCertificate: true,
-//   },
-//   requestTimeout: 120000
-// };
-
-// const poolPromise = new sql.ConnectionPool(config)
-//   .connect()
-//   .then(pool => {
-//     console.log("✅ Kết nối SQL Server thành công");
-//     return pool;
-//   })
-//   .catch(err => {
-//     console.error("❌ Lỗi kết nối SQL Server:", err);
-//     throw err;
-//   });
-
-// module.exports = {
-//   sql, poolPromise
-// };
-
-
-// db.js
 require('dotenv').config();
 const sql = require('mssql');
 
@@ -121,3 +87,5 @@ process.on('SIGINT', async () => {
 });
 
 module.exports = { sql, poolPromise };
+
+
